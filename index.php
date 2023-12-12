48618eb62fa24b74f8459c369926d6d22e9136bd
cassebonbon <claddupont9@gmail.com>
2023-12-12 16:16:28 +0100

    commentaire

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Ma page</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">À propos</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <section>
+      <h2>Titre de la section</h2>
+      <p>Contenu de la section</p>
+    </section>
+
+    <section>
+      <h2>Titre de la section</h2>
+      <p>Contenu de la section</p>
+    </section>
+
+    <section>
+      <h2>Titre de la section</h2>
+      <p>Contenu de la section</p>
+    </section>
+  </main>
+
+  <footer>
+    <p>© 2022 Mon Site | Tous droits réservés</p>
+  </footer>
+</body>
+</html>
